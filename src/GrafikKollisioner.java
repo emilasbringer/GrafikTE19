a1610349a1031fb518193d9b49404195c66d00d2
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2021-03-26 10:27:33 +0100

    Updated collisions

--- src/GrafikKollisioner.java
+++ src/GrafikKollisioner.java
@@ -9,13 +9,13 @@ import java.io.IOException;
 
 /**
  * This is a class
- * Created 2021-03-19
+ * Created 2021-03-23
  *
  * @author Magnus Silverdal
  */
-public class Grafik extends Canvas implements Runnable{
-    private int width = 800;
-    private int height = 600;
+public class GrafikKollisioner extends Canvas implements Runnable{
+    private int width = 400;
+    private int height = 300;
 
     private Thread thread;
     int fps = 30;
@@ -23,17 +23,19 @@ public class Grafik extends Canvas implements Runnable{
 
     private BufferStrategy bs;
 
-    private int houseX, houseY;
+    private Rectangle house;
     private int houseVX, houseVY;
 
-    private int treeX, treeY, treeVX, treeVY;
+    private Rectangle tree;
+    private int treeVX, treeVY;
 
-    private int marioX, marioY, marioVX, marioVY;
+    private Rectangle mario;
+    private int marioVX, marioVY;
 
-    private BufferedImage mario;
+    private BufferedImage marioimg;
 
 
-    public Grafik() {
+    public GrafikKollisioner() {
         JFrame frame = new JFrame("A simple painting");
         this.setSize(width,height);
         frame.add(this);
@@ -45,43 +47,46 @@ public class Grafik extends Canvas implements Runnable{
         isRunning = false;
 
         try {
-            mario = ImageIO.read(new File("supermario2.png"));
+            marioimg = ImageIO.read(new File("supermario2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        houseX = 300;
-        houseY = 150;
+        house = new Rectangle(300,150,50,50);
         houseVX = 1;
         houseVY = 0;
 
-        treeX = 200;
-        treeY = 200;
+        tree  =  new Rectangle(400,200,20,40);
         treeVX = 0;
         treeVY = 0;
 
-        marioX = 0;
-        marioY = 0;
+        mario = new Rectangle(200,0,96,96);
         marioVX = 2;
         marioVY = 2;
     }
 
     public void update() {
-        houseX += houseVX;
-        if (houseX > width){
+        house.x += houseVX;
+        if (house.x > width-house.width){
             houseVX = -1;
         }
-        if (houseX < 0 ) {
+        if (house.x < 0 ) {
             houseVX = 1;
         }
-        marioX += marioVX;
-        marioY += marioVY;
-        if (marioX < 0 || marioX > width-80)
+        mario.x += marioVX;
+        mario.y += marioVY;
+        if (mario.x < 0 || mario.x > width-80)
             marioVX = -marioVX;
-        if (marioY < 0 || marioY > height-80)
+        if (mario.y < 0 || mario.y > height-80)
             marioVY = -marioVY;
-        treeX += treeVX;
-        treeY += treeVY;
+
+        // Det här är inte rätt studsning. Dels blir det åt fel håll, dels kan mario fastna i huset.
+        if (mario.intersects(house)) {
+            System.out.println("Hit!");
+                marioVX = -marioVX;
+                marioVY = -marioVY;
+
+        }
     }
 
     public void draw() {
@@ -95,15 +100,15 @@ public class Grafik extends Canvas implements Runnable{
         update();
         g.setColor(Color.WHITE);
         g.fillRect(0,0,width,height);
-        drawHouse(g, houseX,houseY);
-        drawTree(g, treeX,treeY);
+        drawHouse(g, house.x,house.y);
+        drawTree(g, tree.x,tree.y);
         drawTree(g, 100,200);
         drawTree(g, 110,200);
         drawTree(g, 120,200);
         drawTree(g, 130,200);
         drawTree(g, 140,200);
         drawTree(g, 150,200);
-        g.drawImage(mario,marioX,marioY,48,48,null);
+        g.drawImage(marioimg,mario.x,mario.y,mario.width,mario.height,null);
         g.dispose();
         bs.show();
     }
@@ -119,19 +124,19 @@ public class Grafik extends Canvas implements Runnable{
 
     private void drawHouse(Graphics g, int x, int y) {
         g.setColor(new Color(0xAA1111));
-        g.fillRect(x, y-40, 50, 40);
+        g.fillRect(x, y, 50, 40);
         g.setColor(new Color(0x444444));
         int[] xcoords = {x-5, x + 25, x + 55};
-        int[] ycoords = {y-40, y - 65, y-40};
+        int[] ycoords = {y, y - 25, y};
         g.fillPolygon(xcoords, ycoords, 3);
-        g.fillRect(x+4,y-35,15,35);
-        g.drawRect(x+25,y-30,20,20);
+        g.fillRect(x+4,y+5,15,35);
+        g.drawRect(x+25,y+10,20,20);
         g.setColor(new Color(0xFFA3DCFA));
-        g.fillRect(x+26,y-29,18,18);
+        g.fillRect(x+26,y+11,18,18);
     }
 
     public static void main(String[] args) {
-        Grafik painting = new Grafik();
+        GrafikKollisioner painting = new GrafikKollisioner();
         painting.start();
     }
 
@@ -247,5 +252,3 @@ public class Grafik extends Canvas implements Runnable{
         }
     }
 }
-
-
